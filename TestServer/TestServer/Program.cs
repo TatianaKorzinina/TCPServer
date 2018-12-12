a47c1ec23ce9dfef4828988be4004864220635f1
Tatiana Korzinina <sambara@yandex.ru>
2018-12-12 18:26:35 +0300

    try catch block added

--- TestServer/TestServer/Program.cs
+++ TestServer/TestServer/Program.cs
@@ -15,8 +15,9 @@ namespace TestServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Multi-Threaded TCP Server Demo");
-            TcpServer server = new TcpServer(5555);
+            Console.WriteLine("enter port number");
+           var port =  Convert.ToInt32(Console.ReadLine());
+            TcpServer server = new TcpServer(port);
 
         }
     }
@@ -26,8 +27,7 @@ namespace TestServer
         private TcpListener _server;
         private Boolean _isRunning;
         private int counter = 0;
-        //добавляю коллекцию клиентов
-
+      
         private List<Client> clients = new List<Client>();
 
         public TcpServer(int port)
@@ -48,13 +48,10 @@ namespace TestServer
                 TcpClient newClient = _server.AcceptTcpClient();
                 counter += 1;
                 // client found.
-                // create a thread to handle communication
 
-                //пытаюсь сделать коллекцию
                 clients.Add(new Client(newClient,counter));
-
-
-
+                
+                // create a thread to handle communication
                 Thread t = new Thread((HandleClient));
                 t.Start(clients[clients.Count-1]);
             }
@@ -70,61 +67,62 @@ namespace TestServer
             // sets two streams
               StreamWriter sWriter = new StreamWriter(client.TcpClient.GetStream(), Encoding.ASCII);
             StreamReader sReader = new StreamReader(client.TcpClient.GetStream(), Encoding.ASCII);
-            // you could use the NetworkStream to read and write, 
-            // but there is no forcing flush, even when requested
+          
 
             Boolean bClientConnected = true;
             String sData = null;
 
             while (bClientConnected)
             {
-
-                //networkStream.Read(bytesFrom, 0, bytesFrom.Length);
-                //var dataFromClient = Encoding.ASCII.GetString(bytesFrom);
-                Stopwatch watch = new Stopwatch();
-                // reads from stream
-                sData = sReader.ReadLine();
-                watch.Start();
-                //string option = null;
-                //string command = sData.Split(':')[0];
-                //if (sData.Contains(':'))
-                //{    
-                //   option = sData.Split(':')[1];
-                //}
-
-                MessageReceived(sData, sWriter, client);
-
-                // shows content on the console.
-                //Console.WriteLine("Client:" + client.Number + sData);
-                
-
-                if (sData == "time")
+                try
                 {
-                    var date = DateTime.Now;
 
-                    // to write something back.
-                    sWriter.WriteLine(date);
-                    sWriter.Flush();
-                }
-                watch.Stop();
-                if (client.Report)
-                {
-                    sWriter.WriteLine("command " + sData+ " compleded in "+ watch.ElapsedMilliseconds+
-                        " ms");
-                    sWriter.Flush();
-                }
 
-                if (client.Log)
-                {
-                    using (StreamWriter stream = new StreamWriter(string.Format("log of {0} client.txt", client.Number),true))
+
+                    Stopwatch watch = new Stopwatch();
+                    // reads from stream
+                    sData = sReader.ReadLine();
+
+                    watch.Start();
+
+                    MessageReceived(sData, sWriter, client);
+
+                    if (sData == "time")
                     {
-                        stream.WriteLine(sData);
+                        var date = DateTime.Now;
+
+                        sWriter.WriteLine(date);
+                        sWriter.Flush();
                     }
 
+                    watch.Stop();
+                    if (client.Report)
+                    {
+                        sWriter.WriteLine("command " + sData + " compleded in " + watch.ElapsedMilliseconds +
+                                          " ms");
+                        sWriter.Flush();
+                    }
+
+                    if (client.Log)
+                    {
+                        using (StreamWriter stream =
+                            new StreamWriter(string.Format("log of {0} client.txt", client.Number), true))
+                        {
+                            stream.WriteLine(sData);
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    break;
+                    
                 }
             }
         }
 
+        // set log и report values
         public static void MessageReceived(string str, StreamWriter writer, Client client)
         {
             if (str == "report:on" || str == "report:off" || str == "log:on"
