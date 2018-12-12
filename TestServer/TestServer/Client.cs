3c5f7511e0163d05c17b7d0ca92d15a038700b8c
Tatiana Korzinina <sambara@yandex.ru>
2018-12-12 17:07:20 +0300

    base functionality. time and reporn commands work

---
+++ TestServer/TestServer/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServer
+{
+    class Client
+    {
+        public int Number { get; set; }
+        public bool Report { get; set; }
+        public bool Log { get; set; }
+        public readonly TcpClient TcpClient;
+
+        public Client(TcpClient cl, int count)
+        {
+            TcpClient = cl;
+            Number = count;
+            Log = false;
+            Report = false;
+        }
+
+    }
+}
